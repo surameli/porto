949d74bebc8e6c7a853f39b39eb710bc04afb51c
surameli <167941245+surameli@users.noreply.github.com>
2024-09-02 01:05:53 +0300

    first

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Profile from '../../components/Banner/Profile/Profile'
+import AboutMe from '../../components/AboutMe/AboutMe'
+import Resume from '../../components/Resume/Resume'
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Profile/>
+    <AboutMe/>
+    <Resume/>
+    </>
+  )
+}
+
+export default Home
